c813a5f14481cbdb9dfb3a7878209ae6a3cac9c3
phongvudeptrai <vup3231@gmail.com>
2026-09-03 14:45:29 +0700

    Fix HTTPS and add nixpacks config for Vite

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use Illuminate\Pagination\Paginator;
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\URL;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -20,6 +21,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        Paginator::useBootstrapFive();
+        if (config('app.env') === 'production') {
+            URL::forceScheme('https');
+        }
     }
 }
